e6347db1eaa3d66c4f357fc04650b0d2791603f8
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-03-15 11:42:50 -0300

    [access->login] config reactiveForms and add method logar.

--- src/app/access/access/login/login.component.ts
+++ src/app/access/access/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { RegisterService } from './../../register.service';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +10,32 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  @Output() renderRegister: EventEmitter<string> = new EventEmitter();
+  @Output() public renderRegister: EventEmitter<string> = new EventEmitter();
+  public formLogin: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private servRegister: RegisterService
+  ) {
+    this.buildMyForm();
+   }
 
   ngOnInit() {
   }
 
   render() {
     this.renderRegister.emit('REGISTER');
+  }
+
+  public logar(): void {
+    this.servRegister.autentication(this.formLogin.controls.email.value, this.formLogin.controls.password.value);
+  }
+
+  private buildMyForm(): void {
+    this.formLogin = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]]
+    });
   }
 
 }
