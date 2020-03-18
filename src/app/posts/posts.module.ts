4a42be1026129d951ae4babeed5277e1bc9e8ea3
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-03-18 10:50:57 -0300

    [post] config add new component.

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PostsRoutingModule } from './posts-routing.module';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { HomeComponent } from './home/home.component';
 import { PublicationsComponent } from './publications/publications.component';
+import { NewPublicationComponent } from './publications/new-publication/new-publication.component';
+
+import { PublicationsService } from './publications.service';
+
 
 @NgModule({
-  declarations: [HomeComponent, PublicationsComponent],
+  declarations: [HomeComponent, PublicationsComponent, NewPublicationComponent],
   imports: [
     CommonModule,
-    PostsRoutingModule
-  ]
+    PostsRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule
+  ],
+  providers: [PublicationsService]
 })
 export class PostsModule { }
